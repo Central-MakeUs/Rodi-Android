cf4516ed36042989f8274ae2042699c7a67490fb
김의현 <hyunkim6790@gmail.com>
2026-06-30 01:22:37 +0900

    build: 앱 버전 1.0.0 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.dororong.rodi"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0-alpha.1"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
