1bdb98a3c29da104a329576fc31ec95fdb954b86
김의현 <hyunkim6790@gmail.com>
2026-07-17 11:57:20 +0900

    fix(ui): 하단 내비게이션 탭 리플 제거
    - 탭 선택 시 사각형 리플을 표시하지 않도록 인터랙션 표시를 제거
    
    (cherry picked from commit 6cc7986c6e5f237c1d40e251620ef7bdbeccc941)

--- core/ui/src/main/java/com/dororong/rodi/core/ui/components/RodiBottomNavigation.kt
+++ core/ui/src/main/java/com/dororong/rodi/core/ui/components/RodiBottomNavigation.kt
@@ -2,6 +2,7 @@ package com.dororong.rodi.core.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -81,7 +83,11 @@ private fun RodiBottomNavigationItem(
         modifier = Modifier
             .width(56.dp)
             .padding(top = 8.dp, bottom = 4.dp)
-            .clickable(onClick = onClick)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick,
+            )
             .semantics {
                 role = Role.Tab
                 this.selected = selected
