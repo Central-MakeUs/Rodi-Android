d2f5bc1699b1c6600b794d5e3263d5f0d3c30ad3
김의현 <hyunkim6790@gmail.com>
2026-08-16 17:37:48 +0900

    build: 1.3.0-alpha03 (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.dororong.rodi"
         minSdk = 30
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.3.0-alpha02"
+        versionCode = 14
+        versionName = "1.3.0-alpha03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
