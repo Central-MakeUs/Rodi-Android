ba0f1ffe594011e194e7af0f37f83aa3da5f5b8f
김의현 <hyunkim6790@gmail.com>
2026-08-22 13:26:17 +0900

    build: 1.4.3 버전 반영

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.dororong.rodi"
         minSdk = 30
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.4.2"
+        versionCode = 21
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
